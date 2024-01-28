edc5ad2c7541adf36c8f68c4838644a5acddeb83
Eun Koo <139302489+EunnyKoo@users.noreply.github.com>
2024-01-28 17:17:59 +0900

    Delete frontend/11-socket directory

--- frontend/11-socket/src/components/Chat.js
+++
@@ -1,8 +0,0 @@
-export default function Chat({ chat }) {
-  return (
-    <div className={`list ${chat.type}-chat`}>
-      <div className="user-id">{chat.content2}</div>
-      <div className="list-content">{chat.content}</div>
-    </div>
-  );
-}
